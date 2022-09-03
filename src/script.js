1e5b3d541388331394819b3489d0f2dce5bf1856
ThiagoC <kirito98.1234@gmail.com>
2022-09-03 15:58:16 -0300

    Cargue la pagina del alien que ya teniamos hecha yo y cris

---
+++ src/script.js
@@ -0,0 +1,7 @@
+let items = ["sona", "julio","moren","samuel","funson","fun","mato","ingle","formi","astri","austri","moneda","cristian","nimus","thanos","avenger","Juana", "Ruben","Carlos","jose","mario"];
+var item = items[Math.floor(Math.random()*items.length)];
+document.getElementById("nombre").innerHTML = "Name: " + item
+
+let Cplanets = ["marte","jupiter","venus","saturno"];
+var planet = Cplanets[Math.floor(Math.random()*Cplanets.length)];
+document.getElementById("Planet").innerHTML = "planeta natal: " + planet
